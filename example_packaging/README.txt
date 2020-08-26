09585c5e8a07c63de474af8ae7a27be3ce3840dc
MandiYang <64307609+MandiYang@users.noreply.github.com>
2020-08-26 18:34:10 +0200

    Add files via upload

---
+++ example_packaging/README.txt
@@ -0,0 +1,14 @@
+# Example Package
+
+This is a simple example package.
+You can make cardgames with it.
+# Install
+
+pip3 install -i https://test.pypi.org/simple/ Yu-Mandi.yang
+
+# Usage
+
+import Yu
+
+
+
---
+++ example_packaging/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+    print('readed')
+    
+setup(name='MYexmaple',
+      version='0.0.1',
+      description='A exmaple package',
+      author='Mandi Yang',
+      author_email='mandi.yang@elev.kungsbacka.se',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/MandiYang/MandiRepo',
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+      ],
+      python_requires='>=3.5',
+      packages=find_packages()
+)
+
