342c932ece8030b0025302fb3a58919f96e34281
MandiYang <64307609+MandiYang@users.noreply.github.com>
2020-10-11 14:00:11 +0200

    Add files via upload

---
+++ example_packaging/README.md
@@ -0,0 +1,13 @@
+# Example Package
+
+This is a simple example package.
+You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+You can make cardgames with it.
+
+# Usage
+
+import MYexample
+
+
+
--- example_packaging/setup.py
+++ example_packaging/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("README.txt", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
     print('readed')
     
 setup(name='MYexmaple',
-      version='0.1',
+      version='0.1.1',
       description='A exmaple package',
       author='Mandi Yang',
-      author_email='mandi.yang@elev.kungsbacka.se',
+      author_email='yangmandi2008@gmail.com',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='https://github.com/MandiYang/MYexample',
@@ -17,6 +17,7 @@ setup(name='MYexmaple',
         "License :: OSI Approved :: MIT License",
       ],
       python_requires='>=3.5',
+      license='MIT',
       packages=find_packages()
 )
 
